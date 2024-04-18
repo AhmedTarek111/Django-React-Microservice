4014c6ad36dfdf1d9188752c56163a8441ef1673
AhmedTarek111 <ahmedtarekalsaudi@gmail.com>
2024-04-18 18:47:20 +0200

    add date joined to the custom user model

--- users/accounts/models.py
+++ users/accounts/models.py
@@ -30,6 +30,7 @@ class CustomUser(AbstractUser):
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['email','username']
