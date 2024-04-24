283c52aaf9ae10d0f6d5e2c0c1c7541f280d7f41
AhmedTarek111 <ahmedtarekalsaudi@gmail.com>
2024-04-24 23:00:39 +0200

    fix superuser creation and login

--- users/accounts/models.py
+++ users/accounts/models.py
@@ -1,42 +1,43 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser,BaseUserManager,PermissionsMixin
+from django.contrib.auth.models import AbstractUser, BaseUserManager , PermissionsMixin
 from django.contrib.auth.hashers import make_password
-class CustomUserManger(BaseUserManager):
-    
-    def create_user(self,email,password=None,username=None,**extra_fields):
-        if not email :
-            raise ValueError('the email Field is required')
-        
-        
-        email = self.normalize_email(email)
-        user = self.model(email=email,**extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user 
-        
-        
-    def create_superuser(self,email,password=None,username=None,**extra_fields):
-        extra_fields.setdefault('is_active',True)
-        extra_fields.setdefault('is_staff',True)
-        extra_fields.setdefault('is_superuser',True)
-        if password:
-            extra_fields['password']= make_password(password)
-        self.create_user(email,**extra_fields)
-        
-        
-        
+from django.contrib.auth.models import BaseUserManager
 
-class CustomUser(AbstractUser,PermissionsMixin):
-    username = models.CharField(max_length=200,unique=True)
-    email = models.EmailField(unique=True)
-    is_active = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-    date_joined = models.DateTimeField(auto_now_add=True)
+
+
+class CustomUserManager(BaseUserManager):
+  def create_user(self,email,password=None,**extra_fields):
+    if not email :
+      raise ValueError('The Email Field is Required')
+    email = self.normalize_email(email)
     
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-    objects = CustomUserManger()
-    
-    def __str__(self):
-        return str(self.email)
+    user = self.model(email=email , **extra_fields)
+    user.set_password(password)
+    user.save(using=self._db)
+    return user
+
+  def create_superuser(self,email,password=None,**extra_fields):
+    extra_fields.setdefault('is_staff',True)
+    extra_fields.setdefault('is_superuser',True)
+    extra_fields.setdefault('is_active',True)
+
+    return self.create_user(email,password,**extra_fields)
+
+
+
+class CustomUser(AbstractUser):
+  username = None
+  email = models.EmailField(unique=True) 
+  username = models.CharField(max_length=25 , null=True , blank=True) 
+  is_active = models.BooleanField(default=False)
+  is_staff = models.BooleanField(default=False)
+  date_joined = models.DateTimeField(auto_now_add=True) 
+
+  
+  objects = CustomUserManager()
+
+  USERNAME_FIELD = 'email' 
+  REQUIRED_FIELDS = []
+
+  def __str__(self):
+    return self.email
