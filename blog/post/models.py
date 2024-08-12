679150afc0e15fb952e00fc39bcede2f0d96478c
AhmedTarek111 <ahmedtarekalsaudi@gmail.com>
2024-08-12 21:34:02 +0300

    add the missing methods for post in post serializer

--- blog/post/models.py
+++ blog/post/models.py
@@ -14,13 +14,22 @@ class Post(models.Model):
     def total_comments(self):
         total_comments = Comments.objects.filter(post = self).count()
         return total_comments
+    def __str__(self):
+        return self.title
     
 class Postlikes(models.Model):
     post = models.ForeignKey(Post,on_delete=models.CASCADE)
     user_id = models.IntegerField()
+    
+    def __str__(self):
+        return self.post
     
 class Comments(models.Model):
     post = models.ForeignKey(Post,on_delete=models.CASCADE)
     content = models.TextField(max_length=2000)
     user_id = models.IntegerField()
     puplish_date = models.DateTimeField(default=timezone.now )
+    
+    def __str__(self):
+        return self.post
+    
--- blog/post/serializers.py
+++ blog/post/serializers.py
@@ -2,9 +2,20 @@ from rest_framework import serializers
 from .models import Post , Comments , Postlikes
 
 class PostSerialzer(serializers.ModelSerializer):
+    total_likes = serializers.SerializerMethodField()
+    total_comments = serializers.SerializerMethodField()
+    
     class Meta:
         model = Post
         fields = ['id','author','title','content','puplish_date']
+        
+        def get_total_likes(self,obj):
+            total_likes = Postlikes.objects.filter(post = obj).count()
+            return total_likes
+        
+        def get_total_comments(self,obj):
+            total_comments = Comments.objects.filter(post = obj).count()
+            return total_comments 
         
 class PostLikesSerializer(serializers.Modelesrializer):
     class Meta:
@@ -15,3 +26,4 @@ class CommentsSerialzer(serializers.Modelserializer):
     class Meta:
         model = Comments
         fields = ['id','post','content','user_id','puplish_date']
+        
